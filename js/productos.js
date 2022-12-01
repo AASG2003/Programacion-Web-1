a4ec740781ea7bed023fde4255c43337e95490bb
AASG <betoxd2003@gmail.com>
2022-12-01 02:23:37 -0400

    implementacion de scripts para el renderizado de los productos, adicion de nuevos estilos, organizacion de los archivos html y mejoras en el codigo

--- js/productos.js
+++ js/productos.js
@@ -1,35 +1,366 @@
-{/* <div class="producto col-12 col-md-6 col-lg-4">
-    <div class="card ">
-        <img src="images/CIDLI2003.jpg" class="card-img-top" alt="Imagen taladro">
-            <div class="card-body">
-                <h5 class="card-title">CIDLI2003</h5>
-                <p class="card-text">Taladro Atornillador a bateria 20V ION-Litio c/2 bateria c/Maleta</p>
-            </div>
-            <ul class="list-group list-group-flush text-start">
-                <li class="list-group-item">Voltage: 20V</li>
-                <li class="list-group-item">No-load speed: 0-450/0-1900min-1</li>
-                <li class="list-group-item">Max. Torque: 55NM</li>
-                <li class="list-group-item">Chuck capacity: 2.0 - 13mm</li>
-            </ul>
-            <div class="card-body">
-                <button onclick="anyadirProductoAlCarrito(event)" class="btn btn-primary" marcador="1">Agregar al carrito</a>
-            </div>
-    </div>
-</div> */}
-const DOMPrimeraCategoria = document.querySelector("#PrimeraCategoria");
-function renderizarProductos(){
-    const producto = document.createElement('div');
-    producto.classList.add('producto', 'col-12', 'col-md-6', 'col-lg-4');
-    const card = document.createElement('div');
-    card.classList.add('card');
+const DBGenerica = [
+    {
+        id: 1,
+        nombre: "CIDLI2003",
+        precio: 1523,
+        img: "CIDLI2003.jpg",
+        descripcion: "Taladro Atornillador a bateria 20V ION-Litio c/2 bateria c/Maleta",
+        datos : [
+            "Voltage: 20v",
+            "No-load speed: 0-450/0-1900min-1",
+            "Max. Torque: 55NM",
+            "Chuck capacity: 2.0 - 13mm"
+        ]
+    },
+    {
+        id: 2,
+        nombre: "CRHLI2201",
+        precio: 1230,
+        img: "CRHLI2201.jpg",
+        descripcion: "Rotomartillo bateria de 20v individual",
+        datos:[
+            "Voltage: 20v",
+            "No-load speed: 0 -1350/min",
+            "Impact rate: 0-4700bpm",
+            "Single Impact force: 1.5J"
+        ]
+
+    },
+    {
+        id: 3,
+        nombre: "CAGLI1152",
+        precio: 726,
+        img: "CAGLI1152.jpg",
+        descripcion: "Amoladora angular de 4 1/2'' (115mm) a bateria 20v",
+        datos:[
+            "Voltage: 20v",
+            "No-load speed: 9000/min",
+            "Disc diameter: 100/115mm",
+            "Spindle thread: M10/M14"
+        ]
+    },
+    {
+        id: 4,
+        nombre: "CSLI1401",
+        precio: 720,
+        img: "CSLI1401.jpg",
+        descripcion: "Sierra circular de 5 1/2'' 140mm x 25.4mm a bateria 20v ion-litio a bateria 20v",
+        datos:[
+            "Voltage: 20v",
+            "No-load speed: 3600/min",
+            "Blade diamater: 1400mm",
+        ]
+    },
+    {
+        id: 5,
+        nombre: "CVLI2001",
+        precio: 451,
+        img: "CVLI2001.jpg",
+        descripcion: "Aspiradora a bateria 20v ion-litio",
+        datos:[
+            "Voltage: 20v",
+            "Capacity: 1L",
+            "Air flow Rate: >7kps"
+        ]
+    },
+    {
+        id: 6,
+        nombre: "HCP08168",
+        precio: 40,
+        img: "HCP08168.jpg",
+        descripcion: "Alicate Univ. Fuerza 6'' 160mm Blister AC",
+        datos : [
+        ]
+    },
+    {
+        id: 7,
+        nombre: "HDCP08188",
+        precio: 45,
+        img: "HDCP08188.jpg",
+        descripcion: "Alicate de corte diagonal 7'' 180mm",
+        datos : [
+        ]
+    },
+    {
+        id: 8,
+        nombre: "HLNP08168",
+        precio: 36,
+        img: "HLNP08168.jpg",
+        descripcion: "Alicate de punta larga 6'' 160mm Blister AC",
+        datos : [
+        ]
+    },
+    {
+        id: 9,
+        nombre: "HPW0808",
+        precio: 51,
+        img: "HPW0808.jpg",
+        descripcion: "Llave Stilson 8'' 200mm ind. abertura max. 27mm",
+        datos : [
+        ]
+    },
+    {
+        id: 10,
+        nombre: "HBC0814",
+        precio: 101,
+        img: "HPC0814.jpg",
+        descripcion: "Corta perno 14'' ind. max de corte 7.0mm",
+        datos : [
+        ]
+    }
     
+]
+const productosHerramientasElectricasDB = [
+    {
+        id: 1,
+        nombre: "CIDLI2003",
+        precio: 1523,
+        img: "CIDLI2003.jpg",
+        descripcion: "Taladro Atornillador a bateria 20V ION-Litio c/2 bateria c/Maleta",
+        datos : [
+            "Voltage: 20v",
+            "No-load speed: 0-450/0-1900min-1",
+            "Max. Torque: 55NM",
+            "Chuck capacity: 2.0 - 13mm"
+        ]
+    },
+    {
+        id: 2,
+        nombre: "CRHLI2201",
+        precio: 1230,
+        img: "CRHLI2201.jpg",
+        descripcion: "Rotomartillo bateria de 20v individual",
+        datos:[
+            "Voltage: 20v",
+            "No-load speed: 0 -1350/min",
+            "Impact rate: 0-4700bpm",
+            "Single Impact force: 1.5J"
+        ]
+
+    },
+    {
+        id: 3,
+        nombre: "CAGLI1152",
+        precio: 726,
+        img: "CAGLI1152.jpg",
+        descripcion: "Amoladora angular de 4 1/2'' (115mm) a bateria 20v",
+        datos:[
+            "Voltage: 20v",
+            "No-load speed: 9000/min",
+            "Disc diameter: 100/115mm",
+            "Spindle thread: M10/M14"
+        ]
+    },
+    {
+        id: 4,
+        nombre: "CSLI1401",
+        precio: 720,
+        img: "CSLI1401.jpg",
+        descripcion: "Sierra circular de 5 1/2'' 140mm x 25.4mm a bateria 20v ion-litio a bateria 20v",
+        datos:[
+            "Voltage: 20v",
+            "No-load speed: 3600/min",
+            "Blade diamater: 1400mm",
+        ]
+    },
+    {
+        id: 5,
+        nombre: "CVLI2001",
+        precio: 451,
+        img: "CVLI2001.jpg",
+        descripcion: "Aspiradora a bateria 20v ion-litio",
+        datos:[
+            "Voltage: 20v",
+            "Capacity: 1L",
+            "Air flow Rate: >7kps"
+        ]
+    }
+];
+
+const productosHerramientasManualesDB = [
+    {
+        id: 6,
+        nombre: "HCP08168",
+        precio: 40,
+        img: "HCP08168.jpg",
+        descripcion: "Alicate Univ. Fuerza 6'' 160mm Blister AC",
+        datos : [
+        ]
+    },
+    {
+        id: 7,
+        nombre: "HDCP08188",
+        precio: 45,
+        img: "HDCP08188.jpg",
+        descripcion: "Alicate de corte diagonal 7'' 180mm",
+        datos : [
+        ]
+    },
+    {
+        id: 8,
+        nombre: "HLNP08168",
+        precio: 36,
+        img: "HLNP08168.jpg",
+        descripcion: "Alicate de punta larga 6'' 160mm Blister AC",
+        datos : [
+        ]
+    },
+    {
+        id: 9,
+        nombre: "HPW0808",
+        precio: 51,
+        img: "HPW0808.jpg",
+        descripcion: "Llave Stilson 8'' 200mm ind. abertura max. 27mm",
+        datos : [
+        ]
+    },
+    {
+        id: 10,
+        nombre: "HBC0814",
+        precio: 101,
+        img: "HPC0814.jpg",
+        descripcion: "Corta perno 14'' ind. max de corte 7.0mm",
+        datos : [
+        ]
+    }
+];
+
+const DOMPrimeraCategoria = document.querySelector("#PrimeraCategoria");
+const DOMSegundoCategoria = document.querySelector("#SegundaCategoria");
+
+function renderizarProductos(){
+    let marcador = 1;
+    productosHerramientasElectricasDB.forEach(cardInfo => {
+
+        const producto = document.createElement('div');
+        producto.classList.add('producto', 'col-12', 'col-md-6', 'col-lg-4');
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', 'images/' + cardInfo.img);
+        img.setAttribute('alt', 'Imagen del producto');
+        img.classList.add('card-img-top');
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        const nombre = document.createElement('h5');
+        nombre.classList.add('card-title');
+        nombre.textContent = cardInfo.nombre;
+
+        const descripcion = document.createElement('p');
+        descripcion.classList.add('card-text');
+        descripcion.textContent = cardInfo.descripcion;
+        cardBody.appendChild(nombre);
+        cardBody.appendChild(descripcion);
+
+        const unlist = document.createElement('ul');
+        unlist.classList.add('list-group', 'list-group-flush', 'text-start');
+
+        for(i = 0; i < cardInfo.datos.length; i++){
+            const listEle = document.createElement('li');
+            listEle.classList.add('list-group-item');
+            listEle.textContent = cardInfo.datos[i];
+            unlist.appendChild(listEle);
+        }
+        // boton de carrito
+        const botonDiv = document.createElement('div');
+        botonDiv.classList.add('card-body');
+
+        const boton = document.createElement('button');
+        boton.setAttribute('onclick', "anyadirProductoAlCarrito(event)");
+        boton.setAttribute('marcador', marcador);
+        boton.classList.add('btn', 'btn-primary');
+        boton.textContent = 'Agregar al carrito';
+        botonDiv.appendChild(boton);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        card.appendChild(unlist);
+        card.appendChild(botonDiv);
+        producto.appendChild(card);
+        DOMPrimeraCategoria.appendChild(producto);
+        marcador++;
+    });
+
+    marcador = 6;
+    productosHerramientasManualesDB.forEach(cardInfo => {
+
+        const producto = document.createElement('div');
+        producto.classList.add('producto', 'col-12', 'col-md-6', 'col-lg-4');
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', 'images/' + cardInfo.img);
+        img.setAttribute('alt', 'Imagen del producto');
+        img.classList.add('card-img-top');
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        const nombre = document.createElement('h5');
+        nombre.classList.add('card-title');
+        nombre.textContent = cardInfo.nombre;
+
+        const descripcion = document.createElement('p');
+        descripcion.classList.add('card-text');
+        descripcion.textContent = cardInfo.descripcion;
+        cardBody.appendChild(nombre);
+        cardBody.appendChild(descripcion);
+
+        const unlist = document.createElement('ul');
+        unlist.classList.add('list-group', 'list-group-flush', 'text-start');
+
+        for(i = 0; i < cardInfo.datos.length; i++){
+            const listEle = document.createElement('li');
+            listEle.classList.add('list-group-item');
+            listEle.textContent = cardInfo.datos[i];
+            unlist.appendChild(listEle);
+        }
+        // boton de carrito
+        const botonDiv = document.createElement('div');
+        botonDiv.classList.add('card-body');
+
+        const boton = document.createElement('button');
+        boton.setAttribute('onclick', "anyadirProductoAlCarrito(event)");
+        boton.setAttribute('marcador', marcador);
+        boton.classList.add('btn', 'btn-primary');
+        boton.textContent = 'Agregar al carrito';
+        botonDiv.appendChild(boton);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        card.appendChild(unlist);
+        card.appendChild(botonDiv);
+        producto.appendChild(card);
+        DOMSegundoCategoria.appendChild(producto);
+        marcador++;
+    });
 }
 
+let carrito = [];
+let carritoTabla = [];
+const divisa = 'Bs.';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMcarritoTabla = document.querySelector("#carritoTabla");
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+
+function anyadirProductoAlCarrito(evento) {
+    carritoTabla.push(evento.target.getAttribute('marcador'));
+    renderizarCarritoTabla();
+
+}
+
+// Crear carrito con tables
 function renderizarCarritoTabla() {
     DOMcarritoTabla.textContent = '';
     const carritoSinDuplicados = [...new Set(carritoTabla)];
     carritoSinDuplicados.forEach((item) => {
-        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+        const miItem = DBGenerica.filter((itemBaseDatos) => {
             return itemBaseDatos.id === parseInt(item);
         });
         const numeroUnidadesItem = carritoTabla.reduce((total, itemId) => {
@@ -53,3 +384,30 @@ function renderizarCarritoTabla() {
     });
     DOMtotal.textContent = calcularTotal();
 }
+
+function borrarItemCarrito(evento) {
+    const id = evento.target.dataset.item;
+    carritoTabla = carritoTabla.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    renderizarCarritoTabla();
+}
+
+function calcularTotal() {
+    return carritoTabla.reduce((total, item) => {
+        const miItem = DBGenerica.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+}
+
+
+function vaciarCarrito() {
+    carritoTabla = [];
+    renderizarCarritoTabla();
+}
+
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+renderizarCarritoTabla();
+renderizarProductos();
